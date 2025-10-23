9862916345394d792dfef7d9269155763b2da7ac
U230709078 <gorkemozden2005@gmail.com>
2025-10-23 11:42:22 +0300

    Committed Week5 Lab 23/10/2025

--- app/src/main/java/com/example/week5/MainActivity.java
+++ app/src/main/java/com/example/week5/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TableRow;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -50,7 +51,23 @@ public class MainActivity extends AppCompatActivity {
         return board[row][column] == 0;
 
     }
+    public int gameEnded(int row, int col) {
+        int symbol = board[row][col];
+        boolean win = true;
+        for(int i=0; i<3; i++){
+            if(board[i][col] != symbol){
+                win = false;
+                break;
+            }
+        }
+        if(win) {
+            return symbol;
+        }
+        //check rows
+        //check columns
 
+        return -1;
+    }
 
 
     class CellListener implements View.OnClickListener {
@@ -65,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
 
             if(!isValidMove(row, column)) {
                 Toast.makeText(MainActivity.this, "Cell is already occupied." , Toast.LENGTH_LONG).show();
+                return;
             }
 
 
@@ -77,7 +95,66 @@ public class MainActivity extends AppCompatActivity {
                 ((Button)v).setText(PLAYER_2);
                 board[row][column] = 2;
             }
+            if(gameEnded(row, column) == -1) {
+                player1_Turn =! player1_Turn;
+            }
+            else if (gameEnded(row, column) == 0) {
+                Toast.makeText(MainActivity.this, "It is a DRAW." , Toast.LENGTH_LONG).show();
+            }
+
+            else if (gameEnded(row, column) == 1) {
+                Toast.makeText(MainActivity.this, "Player 1 Wins." , Toast.LENGTH_LONG).show();
+            }
+
+            else {
+                Toast.makeText(MainActivity.this, "Player 2 Wins." , Toast.LENGTH_LONG).show();
+            }
 
         }
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putBoolean("player1Turn" , player1_Turn);
+        byte [] boardSingle = new byte[9];
+        for (int i=0; i<3; i++) {
+            for (int j=0; j<3; j++) {
+                boardSingle[i * 3 + j] = board[i][j];
+            }
+        }
+        outState.putByteArray("board" , boardSingle);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        player1_Turn = savedInstanceState.getBoolean("player1Turn");
+        byte [] boardSingle = savedInstanceState.getByteArray("board");
+
+        for (int i=0; i<9; i++) {
+            board[i/3][i%3] = boardSingle[i];
+        }
+
+        TableLayout table = findViewById(R.id.board);
+
+        for(int i=0; i<3; i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+
+            for (int j=0; j<3; j++) {
+                Button button = (Button) row.getChildAt(j);
+
+                if(board[i][j] == 1) {
+                    button.setText("X");
+                }
+
+                else if (board[i][j] == 2) {
+                    button.setText("O");
+                }
+            }
+        }
+
+    }
+
+
 }
